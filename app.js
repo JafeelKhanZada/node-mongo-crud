624d3072cfeda6204ae4fce6c3ea40efdff8bd66
Jafeel KhanZada <jafeelkhanzada@gmail.com>
2022-09-28 18:48:54 +0500

    Vercel configure

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const middleware = require("./middleware");
+const middleware = require("./src/middleware");
 const PORT = process.env.PORT || 4000;
 const app = express();
-const Router = require("./router");
-let DB = require("./config/db");
+const Router = require("./src/router");
+let DB = require("./src/config/db");
 middleware(app);
 DB = new DB.DBConfig();
 Router(app);
